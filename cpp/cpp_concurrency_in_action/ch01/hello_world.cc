834e6a994ae848eb4fb72b46e2b46277866542a6
OkabeRintarou <2573269786@qq.com>
2018-03-28 20:32:42 +0800

    ch01: introduction

---
+++ cpp/cpp_concurrency_in_action/ch01/hello_world.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+
+void hello() {
+        cout << "Hello,World!" << endl;
+}
+
+int main() {
+        thread t(hello);
+        t.join();
+        return 0;
+}

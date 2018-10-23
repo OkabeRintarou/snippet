cf5c1b143dcf6018d25010a496d5ea324223265f
OkabeRintarou <2573269786@qq.com>
2018-10-23 17:41:37 +0800

    简单门逻辑

---
+++ verilog/计算机原理与设计/ch02/cmosnot_test.v
@@ -0,0 +1,18 @@
+`include "cmosnot.v"
+module cmosnot_test;
+	reg a,b;
+	wire f;
+	cmosnot not_1 (f,a);
+	initial begin
+				a = 1;
+		#1	a = 0;
+		#1	a = 1;
+		#1 $finish;
+	end
+
+	initial begin
+		$monitor ($time,": a = %b;",a,"  f = %b;",f);
+		$dumpfile("cmosnot.vcd");
+		$dumpvars;
+	end
+endmodule

f99a991ea8224e7ef56813433dcaa1a3755e5d7f
OkabeRintarou <2573269786@qq.com>
2018-12-07 22:41:16 +0800

    Chapter04 Statements

---
+++ lua/Programming_In_Lua_3rd_edition/Ch04_Statements/exe.lua
@@ -0,0 +1,25 @@
+-- first
+--[[
+while true do
+end
+]]
+
+-- second
+--[[
+for i = 1, math.huge do
+end
+]]
+
+-- third
+--[[
+repeat 
+until false
+]]
+
+--fourth
+--[[
+::label::
+	goto label
+]]
+
+

01303fe13e6b07f9f9de00aed8a0119bd76998c9
OkabeRintarou <HiyajoMaho@foxmail.com>
2021-09-19 23:00:14 +0800

    cowboy: rebar3 hello world example

---
+++ erlang/cowboy/rebar3_hello_erlang/hello_server/apps/hello_server/src/hello_handler.erl
@@ -0,0 +1,11 @@
+-module(hello_handler).
+-behavior(cowboy_handler).
+-export([init/2]).
+
+init(Req, State) ->
+	Req1 = cowboy_req:reply(
+			 200,
+			 #{<<"content-type">> => <<"text/plain">>},
+			 <<"hello,world">>,
+			 Req),
+	{ok, Req1, State}.
---
+++ erlang/cowboy/rebar3_hello_erlang/hello_server/apps/hello_server/src/hello_server_app.erl
@@ -0,0 +1,26 @@
+%%%-------------------------------------------------------------------
+%% @doc hello_server public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(hello_server_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+	Dispatch = cowboy_router:compile([
+		{<<"localhost">>, [{<<"/">>, hello_handler, [] }]}
+	]),
+	{ok, _} = cowboy:start_clear(
+				hello_listener,
+				[{port, 8080}],
+				#{env => #{dispatch => Dispatch}}
+			   ),
+    hello_server_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions

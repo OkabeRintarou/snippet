c9ef3078bf8ca0ed2d25965bf1c7bcb31f955272
OkabeRintarou <HiyajoMaho@foxmail.com>
2021-10-17 21:13:21 +0800

    OPTInAction: tr_server

---
+++ erlang/OTPInAction/tr_server.erl
@@ -0,0 +1,73 @@
+-module(tr_server).
+
+-behavior(gen_server).
+
+%% API
+-export([get_count/0]).
+-export([stop/0, start_link/0, start_link/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE).
+-define(DEFAULT_PORT, 1055).
+-record(state, {port, lsock, request_count = 0}).
+
+stop() ->
+    gen_server:call(?SERVER, stop).
+
+start_link(Port) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
+
+start_link() ->
+    start_link(?DEFAULT_PORT).
+
+init([Port]) ->
+    {ok, LSock} = gen_tcp:listen(Port, [{active, true}]),
+    {ok, #state{port = Port, lsock = LSock}, 0}.
+
+handle_call(get_count, _From, State) ->
+    {reply, {ok, State#state.request_count}, State}.
+
+handle_cast(stop, State) ->
+    {stop, normal, State}.
+
+handle_info({tcp, Socket, RawData}, State) ->
+    do_rpc(Socket, RawData),
+    RequestCount = State#state.request_count,
+    {noreply, State#state{request_count = RequestCount + 1}};
+handle_info(timeout, #state{lsock = LSock} = State) ->
+    {ok, _Sock} = gen_tcp:accept(LSock),
+    {noreply, State};
+handle_info(_Msg, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+get_count() ->
+    gen_server:call(?SERVER, get_count).
+
+do_rpc(Socket, RawData) ->
+    try
+        {M, F, A} = split_out_mfa(RawData),
+        Result = apply(M, F, A),
+        gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Result]))
+    catch
+        _Class:Err ->
+            gen_tcp:send(Socket, io_lib:write("~p~n", [Err]))
+    end.
+
+split_out_mfa(RawData) ->
+    MFA = re:replace(RawData, "\r\n$", "", [{return, list}]),
+    {match, [M, F, A]} =
+        re:run(MFA,
+            "(.*):(.*)\s*\\((.*)\s*\\)\s*.\s*$",
+            [{capture, [1,2,3],list}, ungreedy]),
+    {list_to_atom(M), list_to_atom(F), args_to_terms(A)}.
+
+args_to_terms(RawArgs) ->
+    {ok, Toks, _Line} = erl_scan:string("[" ++ RawArgs ++ "]. ", 1),
+    {ok, Args} = erl_parse:parse_term(Toks),
+    Args.

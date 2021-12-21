f7c0f66d22fda03e3b75c639f15c84ee50d92a15
OkabeRintarou <HiyajoMaho@foxmail.com>
2021-12-21 16:51:50 +0800

    learn-opengl: add bash script to install dependencies
    Signed-off-by: OkabeRintarou <HiyajoMaho@foxmail.com>

---
+++ cpp/opengl/learnopengl.com/install-dependencies.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+if [ "$(id -u)" != "0" ]; then
+	echo "Run the script as root"
+	exit 1
+fi
+
+function install_glfw()
+{
+	echo "install glfw..."
+	rm -rf ./glfw
+	git clone "https://github.com/glfw/glfw"
+	cd ./glfw
+	git checkout tags/3.3.6 -b v3.3.6
+	apt -y install libwayland-dev libxkbcommon-dev wayland-protocols extra-cmake-modules xorg-dev
+	mkdir build && cd build
+	cmake -DCMAKE_BUILD_TYPE=Release ..
+	make -j16
+	make install
+	m -rf ./glfw
+	echo "Done"
+}
+
+if [ ! -d "/usr/local/include/GLFW" ]; then
+	install_glfw
+fi
+
+function install_glew()
+{
+	echo "install glew..."
+	rm -rf ./glew-2.1.0
+	wget https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0.zip/download -O glew-2.1.0.zip
+	unzip glew-2.1.0.zip && cd glew-2.1.0
+	apt install -y libxmu-dev libxi-dev libgl-dev
+	cd glew-2.1.0 && make -j16
+	make install
+	cd ..
+	rm -rf glew-2.1.0.zip glew-2.1.0
+	echo "Done"
+}
+
+if [ ! -d "/usr/local/include/GLEW" ]; then
+	install_glew
+fi

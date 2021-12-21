650c11a225492f2a46f640eb81bf876c6f44a3e9
OkabeRintarou <HiyajoMaho@foxmail.com>
2021-12-21 21:39:39 +0800

    DeepLearningFromScratch: fix type
    Signed-off-by: OkabeRintarou <HiyajoMaho@foxmail.com>

--- python/DeepLearningFromScratch/mnist.py
+++ python/DeepLearningFromScratch/mnist.py
@@ -72,8 +72,8 @@ def init_mnist():
         pickle.dump(dataset, f, -1)
     print('Done!')
 
-def _change_ont_hot_label(X):
-    T = np.zeros(X.size, 10)
+def _change_one_hot_label(X):
+    T = np.zeros((X.size, 10))
     for idx, row in enumerate(T):
         row[X[idx]] = 1
     return T

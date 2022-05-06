374d7c064f549d1f22a8de8c341570142fba0ae0
OkabeRintarou <HiyajoMaho@foxmail.com>
2022-05-07 00:00:33 +0800

    elixir: otp guide
    Signed-off-by: OkabeRintarou <HiyajoMaho@foxmail.com>

---
+++ elixir/kv_umbrella/apps/kv/test/kv/regitry_test.exs
@@ -0,0 +1,38 @@
+defmodule KV.RegistryTest do
+  use ExUnit.Case, async: true
+
+  setup context do
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
+  end
+
+  test "spawn buckets", %{registry: registry} do
+    assert KV.Registry.lookup(registry, "shopping") == :error
+
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    KV.Bucket.put(bucket, "milk", 1)
+    assert KV.Bucket.get(bucket, "milk") == 1
+  end
+
+  test "remove buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+
+    KV.Registry.create(registry, "bogus")
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "remove bucket on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Stop the bucket with non-normal reason
+    Agent.stop(bucket, :shutdown)
+
+    KV.Registry.create(registry, "bogus")
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+end

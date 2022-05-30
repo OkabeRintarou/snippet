edc2ebd3ae8a7a724b0e6ab147bd87aad20e450c
OkabeRintarou <HiyajoMaho@foxmail.com>
2022-05-30 14:27:32 +0800

    LearnOpenGL: add some utility classes
    Signed-off-by: OkabeRintarou <HiyajoMaho@foxmail.com>

---
+++ cpp/opengl/learnopengl.com/Common/Shader.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <cassert>
+#include "Object.h"
+#include "Result.h"
+#include "ShaderManager.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader : public Object {
+public:
+  Shader(shader_source src, const char *vertex_src, const char *frag_src);
+  ~Shader();
+  void use_program();
+
+  std::string message() const override {
+    if (!is_valid()) {
+         return error_msg_;
+    }
+    return Object::message();
+  }
+
+  void set_bool(const std::string &name, bool value) const {
+    set_bool(name.c_str(), value);
+  }
+
+  void set_bool(const char *name, bool value) const {
+    GLint loc = glGetUniformLocation(id_, name);
+    assert(loc != GL_INVALID_VALUE && loc != GL_INVALID_OPERATION);
+    glUniform1i(loc, value ? 1 : 0);
+  }
+
+  void set_int(const std::string &name, int value) const {
+    set_int(name.c_str(), value);
+  }
+
+  void set_int(const char *name, int value) const {
+    GLint loc = glGetUniformLocation(id_, name);
+    assert(loc != GL_INVALID_VALUE && loc != GL_INVALID_OPERATION);
+    glUniform1i(loc, value);
+  }
+
+  void set_float(const std::string &name, float value) const {
+    set_int(name.c_str(), value);
+  }
+
+  void set_float(const char *name, float value) const {
+    GLint loc = glGetUniformLocation(id_, name);
+    assert(loc != GL_INVALID_VALUE && loc != GL_INVALID_OPERATION);
+    glUniform1f(loc, value);
+  }
+
+  void set_vec2(const char *name, const glm::vec2 &vec2) {
+    GLint loc = glGetUniformLocation(id_, name);
+    assert(loc != GL_INVALID_VALUE && loc != GL_INVALID_OPERATION);
+    glUniform2fv(loc, 1, glm::value_ptr(vec2));
+  }
+
+  void set_mat4(const char *name, const glm::mat4 &mat4) {
+    GLint loc = glGetUniformLocation(id_, name);
+    assert(loc != GL_INVALID_VALUE && loc != GL_INVALID_OPERATION);
+    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(mat4));
+  }
+private:
+  std::string error_msg_;
+  GLuint vertex_shader_ = 0u;
+  GLuint frag_shader_ = 0u;
+};

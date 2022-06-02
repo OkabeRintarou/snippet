d1cfa237f65fb09021c7aa3c202447f7f8fb36cd
OkabeRintarou <HiyajoMaho@foxmail.com>
2022-06-02 15:05:36 +0800

    LearnOpenGL: Lighting maps
    Signed-off-by: OkabeRintarou <HiyajoMaho@foxmail.com>

---
+++ cpp/opengl/learnopengl.com/Resources/Shader/LightingMaps_Frag.glsl
@@ -0,0 +1,42 @@
+#version 330 core
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+};
+
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 frag_pos;
+in vec3 normal;
+in vec2 tex_coords;
+
+out vec4 color;
+
+uniform vec3 view_pos;
+uniform Material material;
+uniform Light light;
+
+void main() {
+    // diffuse
+    vec3 norm = normalize(normal);
+    vec3 light_dir = normalize(light.position - frag_pos);
+    float diff = max(dot(norm, light_dir), 0.0f);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, tex_coords));
+    // ambient
+    vec3 ambient = light.ambient * texture(material.diffuse, tex_coords).rgb;
+    // specular
+    vec3 view_dir = normalize(view_pos - frag_pos);
+    vec3 reflect_dir = reflect(-light_dir, norm);
+    float spec = pow(max(dot(view_dir, reflect_dir), 0.0f), material.shininess);
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, tex_coords));
+
+    vec3 result = ambient + diffuse + specular;
+    color = vec4(result, 1.0f);
+}

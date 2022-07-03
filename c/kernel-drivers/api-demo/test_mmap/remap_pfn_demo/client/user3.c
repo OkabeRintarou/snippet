953ca5880e6d76e45c62be37d9b2485a8ffecdbe
OkabeRintarou <HiyajoMaho@foxmail.com>
2022-07-03 22:02:06 +0800

    kernel-driver: test remap_pfn_range
    Signed-off-by: OkabeRintarou <HiyajoMaho@foxmail.com>

---
+++ c/kernel-drivers/api-demo/test_mmap/remap_pfn_demo/client/user3.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#define PAGE_SIZE (4 * 1024)
+#define BUF_SIZE (16 * PAGE_SIZE)
+#define OFFSET (16 * PAGE_SIZE)
+
+int main(int argc, char *argv[]) {
+	int fd;
+	char *addr = NULL;
+
+	fd = open("/dev/remap_pfn", O_RDWR);
+	if (fd < 0) {
+		fprintf(stderr, "failed to open, errno = %s[%d]\n", strerror(errno), errno);
+		return -1;
+	}
+	addr = mmap(NULL, BUF_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_LOCKED, fd, OFFSET); 
+	if (addr == MAP_FAILED) {
+		fprintf(stderr, "failed to mmap, errno = %s[%d]\n", strerror(errno), errno);
+		return -1;
+	}
+
+	sprintf(addr, "I am %s\n", argv[0]);
+
+	for (;;) {
+		sleep(1);
+	}
+
+	munmap(addr, BUF_SIZE);
+	close(fd);
+	return 0;
+}

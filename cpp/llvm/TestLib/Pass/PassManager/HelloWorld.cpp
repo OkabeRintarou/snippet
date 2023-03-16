ed0ccd6778e1da2284371186ff282d87bd8778e6
OkabeRintarou <Yuliang.Shi@outlook.com>
2023-03-16 22:26:53 +0800

    llvm: add new pass manager hello world demo
    Signed-off-by: OkabeRintarou <Yuliang.Shi@outlook.com>

---
+++ cpp/llvm/TestLib/Pass/PassManager/HelloWorld.cpp
@@ -0,0 +1,50 @@
+#include <llvm/ADT/StringRef.h>
+#include <llvm/IR/PassManager.h>
+#include <llvm/Passes/PassBuilder.h>
+#include <llvm/Passes/PassPlugin.h>
+#include <llvm/Support/raw_ostream.h>
+
+namespace {
+
+using namespace llvm;
+
+void visitor(Function &F) {
+  errs() << "Hello from: " << F.getName() << '\n';
+  errs() << "  number of arguments: " << F.arg_size() << "\n";
+}
+
+class HelloWorld : public PassInfoMixin<HelloWorld> {
+public:
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
+    visitor(F);
+    return PreservedAnalyses::all();
+  }
+
+  // Without isRequired returning true, this pass will be skipped for functions
+  // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+  // all functions with optnone
+  static bool isRequired() { return true; }
+};
+
+} // namespace
+
+// New PM Registration
+llvm::PassPluginLibraryInfo getHelloWorldPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "HelloWorld", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](llvm::StringRef Name, llvm::FunctionPassManager &FPM,
+                   llvm::ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "hello-world") {
+                    FPM.addPass(HelloWorld());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getHelloWorldPluginInfo();
+}
